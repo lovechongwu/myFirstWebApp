b6d55be6028b57da4cb1d6755163476d82a31d42
zhumeng <2856814420@qq.com>
2017-08-31 09:07:11 +0800

    第一次提交

---
+++ src/page/login.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="loginDiv">
+    <div class="logo"></div>
+    <form class="loginContent">
+      <div class="mb20">
+        <input type="text" placeholder="请输入用户名"/>
+      </div>
+      <div class="mb20">
+        <input type="text" placeholder="请输入密码"/>
+      </div>
+      <div>
+        <button>登&emsp;录</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scope>
+  .loginDiv{
+    width: 100%;
+    height: 100%;
+    padding: 0 20px;
+    overflow: hidden;
+    .logo{
+      width: 2rem;
+      height: 2rem;
+      margin: 1.5rem auto 1rem;
+      background: #ccc;
+      -webkit-border-radius: 2rem;
+      -moz-border-radius: 2rem;
+      border-radius: 2rem;
+    }
+    .mb20{
+      margin-bottom: 20px;
+    }
+    .loginContent input{
+      width: 100%;
+      height: 1rem;
+      border-bottom: 1px solid #ff6528;
+      padding-left: 0.125rem;
+    }
+    .loginContent button{
+      width: 100%;
+      height: 1rem;
+      background: #ff6528;
+      color: #fff;
+      -webkit-border-radius: 0.125rem;
+      -moz-border-radius: .125rem;
+      border-radius: .125rem;
+      margin-top: 1rem;
+    }
+  }
+</style>
